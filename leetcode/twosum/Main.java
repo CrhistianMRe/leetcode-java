262f9007bab73b7f94c74906773007e2a09e13bb
CrhistianMRe <came.29meca@gmail.com>
2025-05-18 14:35:50 +0800

    Add main directory and first leetcode problem solved

---
+++ leetcode/twosum/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main
+ */
+public class Main {
+
+    
+
+    public static void main(String[] args) {
+    Solution solution = new Solution(); 
+    int num[] = {2,7,11,15};
+    solution.twoSum(num, 9);
+
+    }
+}
