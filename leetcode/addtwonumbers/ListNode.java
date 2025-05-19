0f5ce0e98c0fb2c6af9b9c547f31da2bde0d490e
CrhistianMRe <came.29meca@gmail.com>
2025-05-19 10:11:30 +0800

    Add exercise #2 not completed

---
+++ leetcode/addtwonumbers/ListNode.java
@@ -0,0 +1,29 @@
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { 
+        this.val = val; 
+    }
+    ListNode(int val, ListNode next) { 
+        this.val = val; 
+        this.next = next; 
+    }
+    public ListNode addTwoNumber(ListNode n1, ListNode n2){
+        ListNode list = new ListNode();
+        while(n1.next.has){}
+
+        String first = String.valueOf(n1.next.next.val) + String.valueOf(n1.next.val) + String.valueOf(n1.val);
+        String second = String.valueOf(n2.next.next.val) + String.valueOf(n2.next.val) + String.valueOf(n2.val);
+
+        String answer = String.valueOf(Integer.valueOf(first)  + Integer.valueOf(second));
+
+
+        list.val = Integer.valueOf(answer.charAt(2));
+        list.next.val = Integer.valueOf(answer.charAt(1));
+        list.next.next.val = Integer.valueOf(answer.charAt(0));
+
+        return list;
+    }
+}
