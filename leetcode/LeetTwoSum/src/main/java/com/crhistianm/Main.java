c6f66026cc6c84dde922c45b63761455a078da18
CrhistianMRe <came.29meca@gmail.com>
2025-05-19 14:04:08 +0800

    Update leettwosum leetcode

---
+++ leetcode/LeetTwoSum/src/main/java/com/crhistianm/Main.java
@@ -0,0 +1,14 @@
+package com.crhistianm;
+
+
+class Main {
+
+    
+
+    public static void main(String[] args) {
+    Solution solution = new Solution();
+    int num[] = {2,7,11,15};
+    solution.twoSum(num, 9);
+
+    }
+}
