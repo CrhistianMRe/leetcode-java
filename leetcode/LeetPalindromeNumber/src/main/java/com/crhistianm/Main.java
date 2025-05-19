e15c69feba3accb65e5dbcc8593c0ff4fad63d1e
CrhistianMRe <came.29meca@gmail.com>
2025-05-19 14:03:44 +0800

    Update LeetPalindromeNumber leetcode

---
+++ leetcode/LeetPalindromeNumber/src/main/java/com/crhistianm/Main.java
@@ -0,0 +1,13 @@
+package com.crhistianm;
+
+class Main {
+
+   public static void main(String[] args) {
+        Solution solution = new Solution();
+        if(solution.isPalindrome(-121)){ 
+            System.out.println("true");
+        }
+        
+        
+    } 
+}
