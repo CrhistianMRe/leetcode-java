d843405b2f15f4230d34e159f7da7b04f8973e26
CrhistianMRe <came.29meca@gmail.com>
2025-05-21 20:41:05 +0800

    Add not finished yet prefix program

---
+++ leetcode/LeetLongestCommonPrefix/src/main/java/com/crhistianm/Main.java
@@ -0,0 +1,13 @@
+package com.crhistianm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] strs = {"", "b"};
+        Solution sol = new Solution();
+
+        System.out.println(sol.longestCommonPrefix(strs));
+        
+
+    }
+}
